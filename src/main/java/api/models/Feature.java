06f116b2b2d82fd738f4b75b46c8eb4675aeb543
NascimentoFrancisco <fmilk9049@gmail.com>
2025-04-28 20:57:06 -0300

    feat: Create entities of the application

---
+++ src/main/java/api/models/Feature.java
@@ -0,0 +1,9 @@
+package api.models;
+
+import api.abstracts.BaseItem;
+import jakarta.persistence.Entity;
+
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+}
---
+++ src/main/java/api/models/News.java
@@ -0,0 +1,9 @@
+package api.models;
+
+import api.abstracts.BaseItem;
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_new")
+public class News extends BaseItem {
+
+}
