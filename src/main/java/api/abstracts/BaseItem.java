0376087ae5e86f54c63a856b5ed5a127acbc7747
NascimentoFrancisco <fmilk9049@gmail.com>
2025-04-28 21:21:02 -0300

    chore: Modifying entity and development environment settings

--- src/main/java/api/abstracts/BaseItem.java
+++ src/main/java/api/abstracts/BaseItem.java
@@ -3,8 +3,10 @@ package api.abstracts;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
-public class BaseItem {
+@MappedSuperclass
+public abstract class BaseItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/api/models/Account.java
+++ src/main/java/api/models/Account.java
@@ -19,7 +19,7 @@ public class Account {
     @Column(precision = 2, scale = 13)
     private BigDecimal balance;
 
-    @Column(precision = 2, scale = 13)
+    @Column(name = "additional_limit", precision = 2, scale = 13)
     private BigDecimal limit;
 
     public Long getId() {
